1ed2f1be758aa5d7490822465f984046ab821901
DariRuProg <dar.onl.work@gmail.com>
2024-02-14 22:14:47 +0100

    Update serp.py

--- serp.py
+++ serp.py
@@ -1,7 +1,8 @@
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+
 #from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
