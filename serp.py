871ecdbf08e9180396fafe9818d490f94ba498ce
DariRuProg <dar.onl.work@gmail.com>
2024-02-14 22:14:22 +0100

    webdriver

--- serp.py
+++ serp.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 #from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
@@ -32,12 +34,16 @@ def search_google_web_automation(query, num_results, json_file_name='durchsuchte
 
 
     chrome_options = Options()
+    
+    chrome_options.add_argument("start-maximized")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option("useAutomationExtension", False)
-    driver = webdriver.Chrome(options=chrome_options)
+    #driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    
     #driver = webdriver.Firefox(options=firefox_options)
 
     stealth(
